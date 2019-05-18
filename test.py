a62ea43aef0bd2f6a8e969a78dcc9971ae886d1b
Peyman Shakari <peyman@shakari.no>
2019-05-18 16:21:20 +0200

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-fig = plt.figure()
-#plot sth
-plt.savefig('test.png')
-
-html = 'Some html head' + '<img src=\'test.png\'>' + 'Some more html'
-
-with open('test.html','w') as f:
-    f.write(html)
